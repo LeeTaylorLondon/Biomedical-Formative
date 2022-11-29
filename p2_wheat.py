2431504b4ab342eefe38785e520d66dfccfb1048
Lee Taylor <lees_taylor_18@yahoo.com>
2022-11-29 06:48:45 +0000

    p2 complete

--- p2_wheat.py
+++ p2_wheat.py
@@ -1,3 +1,9 @@
+import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.neighbors import KNeighborsRegressor
 from imports import *
 
 
@@ -18,63 +24,64 @@ def txt_to_csv():
         f.writelines(csv)
 
 
-''' 
-classify wheat seed varieties to Kama, Rosa, 
-and Canadian with kNN; plot the learning curves
-(accuracy) for best k
-'''
-
 data = pd.read_csv("Data/wheat.csv")
+# data = data.drop(['perimeter'], axis=1)
 corr_matrix = data.loc[:].corr()
 print(corr_matrix)
 
 
-''' WIP Drop highly correlated variables '''
-pass
+x = np.array(data['asymmetry_coefficient'])
+y = np.array(data['type'])
+X, Y = [], []
+for elmx, elmy in zip(x, y):
+    X.append([elmx])
+    Y.append([elmy])
+X = np.array(X)
+Y = np.array(Y)
+print(X.shape, Y.shape)
 
 
-# Separating the dependent and independent data variables into two data frames.
-X = data.drop(['type'], axis=1)
-Y = data['type']
+def model():
+    model = KNeighborsRegressor(algorithm='auto', n_neighbors=2,
+                                p=3, weights='uniform', n_jobs=8)
+    return model
 
-# Splitting the dataset into 80% training data and 20% testing data.
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.20, random_state=0)
-
-
-# n_neighbors = 3
-# # y = np.sin(X).ravel()
-# for i, weights in enumerate(["uniform", "distance"]):
-#     knn = neighbors.KNeighborsRegressor(n_neighbors, weights=weights)
-#     y_ = knn.fit(X_train, Y_train).predict(X_test)
-#     plt.subplot(2, 1, i + 1)
-#     print(X_train.shape, Y_train.shape)
-#     # plt.scatter(X_test, Y_test, color="darkorange", label="data")
-#     plt.plot(X_test, y_, color="navy", label="prediction")
-#     plt.axis("tight")
-#     plt.legend()
-#     plt.title("KNeighborsRegressor (k = %i, weights = '%s')" % (n_neighbors, weights))
-# plt.tight_layout()
-# plt.show()
-
-
-def model_fit():
-    def mape(y_actual, y_predicted):
-        return np.mean(np.abs((y_actual - y_predicted)/y_actual))*100
-
+def fit(model):
     # Building the KNN Model on our dataset
-    KNN_model = KNeighborsRegressor(n_neighbors=3).fit(X_train, Y_train)
+    model.fit(X_train, Y_train)
+    return model
+
+def predict(KNN_model):
     # Prediction
-    KNN_predict = KNN_model.predict(X_test)  # Predictions on Testing data
+    prediction = KNN_model.predict(X_test)  # Predictions on Testing data
+    return prediction
 
-    plt.plot(KNN_predict, Y_test)
-    plt.show()
+def stats(model, X, Y, pred_y):
+    score = model.score(X, Y)
+    print(score)
 
-    # Using MAPE error metrics to check for the error rate and accuracy level
-    KNN_MAPE = mape(Y_test, KNN_predict)
-    Accuracy_KNN = 100 - KNN_MAPE
-    print("MAPE: ", KNN_MAPE)
-    print('Accuracy of KNN model: {:0.2f}%.'.format(Accuracy_KNN))
+    mse = mean_squared_error(Y, pred_y)
+    print("Mean Squared Error:", mse)
+
+    rmse = math.sqrt(mse)
+    print("Root Mean Squared Error:", rmse)
 
 
 if __name__ == '__main__':
-    model_fit()
+    ''' 
+    classify wheat seed varieties to Kama, Rosa, 
+    and Canadian with kNN; plot the learning curves
+    (accuracy) for best k
+    '''
+
+    m = model()
+    m.fit(X, Y)
+    p = m.predict(X)
+
+    stats(m, X, Y, p)
+
+    x_ax = range(len(X))
+    plt.scatter(x_ax, y, s=5, color="blue", label="original")
+    plt.plot(x_ax, p, lw=1.5, color="red", label="predicted")
+    plt.legend()
+    plt.show()
